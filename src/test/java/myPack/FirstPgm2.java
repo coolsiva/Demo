319d9eb96ef7aaa628f4429699a904b70d1368ab
siva <coolsiva1990@gmail.com>
2020-06-22 22:03:14 +0530

    first commit

---
+++ src/test/java/myPack/FirstPgm2.java
@@ -0,0 +1,19 @@
+package myPack;
+
+import org.testng.annotations.Test;
+
+public class FirstPgm2 {
+
+	@Test
+	public void test_1()
+	{
+		System.out.println("Hello");
+		System.out.println("siva");
+	}
+
+	@Test
+	public void test_2()
+	{
+		System.out.println("siva");
+	}
+}
